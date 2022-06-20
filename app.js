98485b9599b213a3929ecae462628c5630ef485e
Chloe <tran.m.chloe@gmail.com>
2022-06-20 10:39:53 -0400

    simplify app.js

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 // initial setup: 
 const express = require('express');
 const app = express(); 
-const PORT = 5000; 
+const PORT = process.env.PORT || 5000; 
 
-// routes: 
+// routes file definitions: 
 const indexRoute = require('./routes/index');
-const testRoute = require('./routes/test');
 const orderConfirmedRoute = require('./routes/order-confirmed');
 const menuRoute = require('./routes/menu');
 const purchaseRoute = require('./routes/purchase');
 const newsletterRoute = require('./routes/newsletter');
 
-// middleware:
-// functions that get called before a request is made 
-// called using app.use
-// if you call it in the main app.js file, 
-// the middle ware is enabled for all requests
-
 // enable JSON use:
 app.use(express.json()); 
 
-// routes:
+// routes mapping:
 app.use('/', indexRoute);
-app.use('/test', testRoute);
 app.use('/order-confirmed', orderConfirmedRoute);
 app.use('/menu', menuRoute); 
 app.use('/purchase', purchaseRoute); 
@@ -30,5 +22,5 @@ app.use('/newsletter', newsletterRoute);
 
 // testing: 
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`server is running on port ${PORT}`);
 })
